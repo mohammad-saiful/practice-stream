85ca8927d863dee2d5bb23bfa0c22b1e4af90b3b
mohammad-saiful <mohammad.saiful@brainstation-23.com>
2025-08-06 19:15:28 +0600

    fix: change the todo controller

--- lib/features/stream/presentation/controller/todo_controller.dart
+++ lib/features/stream/presentation/controller/todo_controller.dart
@@ -1,5 +1,9 @@
+import 'dart:async';
 import '../../../../core/stream/todo_controller_stream.dart';
+import '../../domain/entities/loading_state.dart';
 import '../../domain/entities/todo_entities.dart';
+
+enum LoadingState { initial, loading, success, error }
 
 class TodoController {
   static final TodoController _instance = TodoController._internal();
@@ -8,13 +12,33 @@ class TodoController {
 
   TodoController._internal();
 
-  late TodoControllerStream _todoStream;
+  List<TodoEntities> todos = [];
+  final TodoControllerStream _todoStream = TodoControllerStream();
+  final StreamController<LoadingStateData> loadingStream =
+      StreamController<LoadingStateData>.broadcast();
+  final StreamController<List<TodoEntities>> todoDataStream =
+      StreamController<List<TodoEntities>>.broadcast();
 
   void init() {
-    _todoStream = TodoControllerStream();
+    loadingStream.add(LoadingStateData());
   }
 
-  Stream<TodoEntities> get stream => _todoStream.stream;
+  Stream<LoadingStateData> get loadingStateStream => loadingStream.stream;
+  Stream<List<TodoEntities>> get stream => todoDataStream.stream;
 
-  void add(TodoEntities todoData) => _todoStream.add(todoData);
+  void add(TodoEntities todoData) {
+    todos.add(todoData);
+    todoDataStream.add(todos);
+  }
+
+  void startListeningToTodos() async {
+    loadingStream.add(LoadingStateData(initial: false, loading: true));
+    _todoStream.stream.listen((todo) {
+      loadingStream.add(
+        LoadingStateData(initial: false, loading: false, success: true),
+      );
+      todos.add(todo);
+      todoDataStream.add(todos);
+    });
+  }
 }
--- lib/features/stream/presentation/view/stream_screen.dart
+++ lib/features/stream/presentation/view/stream_screen.dart
@@ -3,6 +3,8 @@ import 'package:flutter/material.dart';
 import 'package:practice_stream/features/stream/domain/entities/todo_entities.dart';
 import 'package:practice_stream/features/stream/presentation/controller/todo_controller.dart';
 
+import '../../domain/entities/loading_state.dart';
+
 class StreamScreen extends StatefulWidget {
   const StreamScreen({super.key});
 
@@ -18,6 +20,9 @@ class _StreamScreenState extends State<StreamScreen> {
   void initState() {
     super.initState();
     _controller = TodoController();
+    WidgetsBinding.instance.addPostFrameCallback((_)  {
+      _controller.startListeningToTodos();
+    });
     _controller.init();
   }
 
@@ -25,48 +30,63 @@ class _StreamScreenState extends State<StreamScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text('Stream')),
-      body: StreamBuilder<TodoEntities>(
-        stream: _controller.stream,
+      body: StreamBuilder<LoadingStateData>(
+        stream: _controller.loadingStateStream,
         builder: (context, snapshot) {
-        if (snapshot.hasData) {
-            todos.add(snapshot.data!);
-
-            return ListView.builder(
-              itemCount: todos.length + 1,
-              itemBuilder: (context, index) {
-                if (index == todos.length) {
-                  return Column(
-                    children: [
-                      const SizedBox(height: 10),
-                      FloatingActionButton(
-                        child: const Icon(Icons.add),
-                        onPressed: () {
-                          _controller.add(
-                            TodoEntities(
-                              id: 0,
-                              title: "this is title",
-                              completed: false,
-                            ),
+          if (snapshot.hasData) {
+            if (snapshot.data!.initial) {
+              return const Text('No Data Found');
+            } else if (snapshot.data!.loading) {
+              return const CircularProgressIndicator();
+            } else if (snapshot.data!.success) {
+              return StreamBuilder<List<TodoEntities>>(
+                stream: _controller.stream,
+                builder: (context, snapshot) {
+                  if (snapshot.hasData) {
+                    return ListView.builder(
+                      itemCount: snapshot.data!.length + 1,
+                      itemBuilder: (context, index) {
+                        if (index == snapshot.data!.length) {
+                          return Column(
+                            children: [
+                              const SizedBox(height: 10),
+                              FloatingActionButton(
+                                child: const Icon(Icons.add),
+                                onPressed: () {
+                                  _controller.add(
+                                    TodoEntities(
+                                      id: 0,
+                                      title: "this is title",
+                                      completed: false,
+                                    ),
+                                  );
+                                },
+                              ),
+                              const SizedBox(height: 10),
+                            ],
                           );
-                        },
-                      ),
-                    ],
-                  );
-                }
-
-                return Card(
-                  child: ListTile(
-                    title: Text(todos[index].title),
-                    trailing: Checkbox(
-                      value: todos[index].completed,
-                      onChanged: (value) {},
-                    ),
-                  ),
-                );
-
-               // return Text(todos[index].title);
-              },
-            );
+                        }
+                        return Card(
+                          child: ListTile(
+                            title: Text(snapshot.data![index].title),
+                            trailing: Checkbox(
+                              value: snapshot.data![index].completed,
+                              onChanged: (value) {},
+                            ),
+                          ),
+                        );
+                      },
+                    );
+                  } else {
+                    return const Text('No Data');
+                  }
+                },
+              );
+            } else if (snapshot.data!.error) {
+              return const Text('Error');
+            } else {
+              return const SizedBox.shrink();
+            }
           } else {
             return const Text('No Data');
           }
